c7eab8a5aeb7746eb99d15bb1ecd3a8e8d327faa
olga-sdev <193170299+olga-sdev@users.noreply.github.com>
2025-07-15 19:27:22 +0200

    Update test_blog.py

--- Autotesting/tests/integration/test_blog.py
+++ Autotesting/tests/integration/test_blog.py
@@ -10,3 +10,16 @@ class BlogTest(TestCase):
         self.assertEqual(len(blog.posts), 1)
         self.assertEqual(blog.posts[0].title, 'Test Post')
         self.assertEqual(blog.posts[0].content, 'Test Content')
+
+
+    def test_json(self):
+        blog = Blog('Test', 'Test Author')
+        blog.create_post('Test Post', 'Test Content')
+        expected = {'title': 'Test',
+                    'author': 'Test Author',
+                    'posts': [
+                        {'title': 'Test Post',
+                         'content': 'Test Content'}]}
+
+        self.assertDictEqual(expected,
+                             blog.json())
