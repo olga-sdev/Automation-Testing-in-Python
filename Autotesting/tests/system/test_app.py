69e36f8479aa1b7b5d5156d05c5d4b74a4768a1d
olga-sdev <193170299+olga-sdev@users.noreply.github.com>
2025-07-18 09:38:26 +0200

    Update test_app.py

--- Autotesting/tests/system/test_app.py
+++ Autotesting/tests/system/test_app.py
@@ -10,3 +10,17 @@ class AppTest(TestCase):
         blog = Blog('Test', 'Test Author')
         app.blogs = {'Test': blog}
       
+    def test_menu_calls_create_blog(self):
+        with patch('builtins.input') as mocked_input:
+            with patch('app.ask_create_blog') as mocked_ask_create_blog:
+                mocked_input.side_effect = ('C', 'Test Blog Create', 'Test Author', 'q')
+
+                app.menu()
+
+                mocked_ask_create_blog.assert_called()
+
+    def test_menu_prints_prompt(self):
+        with patch('builtins.input', return_value='q') as mocked_input:
+            app.menu()
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+            
