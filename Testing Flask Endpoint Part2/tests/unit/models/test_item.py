793b4ac9da446f41585d7fb15165703b5b7fded3
olga-sdev <193170299+olga-sdev@users.noreply.github.com>
2025-08-01 22:28:02 +0200

    Update test_item.py

--- Testing Flask Endpoint Part2/tests/unit/models/test_item.py
+++ Testing Flask Endpoint Part2/tests/unit/models/test_item.py
@@ -13,3 +13,15 @@ class ItemTest(UnitBaseTest):  # import UnitBaseTest for better performance
                          "The price of the item after creation does not equal the constructor argument.")
         self.assertEqual(item.store_id, 1)
         self.assertIsNone(item.store)
+        
+    def test_item_json(self):
+        item = ItemModel('test', 19.99)
+        expected = {
+            'name': 'test',
+            'price': 19.99
+        }
+
+        self.assertEqual(
+            item.json(),
+            expected,
+            "The JSON export of the item is incorrect. Received {}, expected {}.".format(item.json(), expected))
