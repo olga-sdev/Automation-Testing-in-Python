4fff65a7064a42150bf41464e92379847124f5d5
olga-sdev <193170299+olga-sdev@users.noreply.github.com>
2025-08-10 09:01:09 +0200

    Update test_store.py

--- Testing Flask Endpoint Part2/tests/integration/models/test_store.py
+++ Testing Flask Endpoint Part2/tests/integration/models/test_store.py
@@ -45,5 +45,22 @@ class StoreTest(BaseTest):
 
         self.assertDictEqual(store.json(), expected)
 
+    def test_store_json_with_item(self):
+        with self.app_context():
+            store = StoreModel('test')
+            item = ItemModel('test_item', 19, 1)
+
+            store.save_to_db()
+            item.save_to_db()
+
+            expected = {
+                'name': 'test',
+                'items': [{'name': 'test_item',
+                           'price': 19}]
+            }
+
+            self.assertDictEqual(store.json(), expected)
+
+
 
     
